918f4e927d072a98f5cff8b541b21879cd5aa519
alfik <alfikmajeed111@gmail.com>
2022-09-12 13:28:55 +0530

    daily commit

--- api/views.py
+++ api/views.py
@@ -1,54 +1,61 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ViewSet
-from api.serializers import PostSerializer,UserSerializer
+from api.serializers import PostSerializer, UserSerializer
 from rest_framework.response import Response
 from api.models import Posts
+from rest_framework import authentication, permissions
 
 
 # Create your views here.
 
-#view To list all Posts
+# view To list all Posts
+#authentication and permissions
 class PostView(ViewSet):
-    def list(self,request,*args,**kwargs):
-        qs=Posts.objects.all()
-        serailizer=PostSerializer(qs,many=True)
+    authentication_classes = [authentication.BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        qs = Posts.objects.all()
+        serailizer = PostSerializer(qs, many=True)
         return Response(data=serailizer.data)
-#create new Post
-    def create(self,request,*args,**kwargs):
-        serializer=PostSerializer(data=request.data)
+
+    # create new Post
+    def create(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data)
         else:
             return Response(data=serializer.errors)
-    def retrieve(self,request,*args,**kwargs):
-        id=kwargs.get('pk')
-        qs=Posts.objects.get(id=id)
-        serializer=PostSerializer(qs)
+
+    def retrieve(self, request, *args, **kwargs):
+        id = kwargs.get('pk')
+        qs = Posts.objects.get(id=id)
+        serializer = PostSerializer(qs)
         return Response(data=serializer.data)
 
-    def update(self,request,*args,**kwargs):
-        id=kwargs.get('pk')
-        instance=Posts.objects.get(id=id)
-        serializer=PostSerializer(instance=instance,data=request.data)
+    def update(self, request, *args, **kwargs):
+        id = kwargs.get('pk')
+        instance = Posts.objects.get(id=id)
+        serializer = PostSerializer(instance=instance, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data)
         else:
             return Response(data=serializer.errors)
 
-    def destroy(self,request,*args,**kwargs):
-        id=kwargs.get('pk')
-        instance=Posts.objects.get(id=id)
+    def destroy(self, request, *args, **kwargs):
+        id = kwargs.get('pk')
+        instance = Posts.objects.get(id=id)
         instance.delete()
-        return Response({"message":"Deleted"})
+        return Response({"message": "Deleted"})
+
 
 class UsersView(ViewSet):
-    def create(self,request,*args,**kwargs):
-        serializer=UserSerializer(data=request.data)
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data)
         else:
             return Response(data=serializer.errors)
-
